cca6ee923b8e9e236ef69baac9bbe6f267dffbe9
PeriBooty <peribooty@pm.me>
2019-07-08 14:25:39 -0400

    Added the ability to change the key/button aliases

--- includes/Input.h
+++ includes/Input.h
@@ -9,20 +9,30 @@ struct GLFWwindow;
 
 namespace UT
 {
+    enum InputActions
+    {
+        Confirm,
+        Back,
+        Menu,
+        Exit
+    };
+
     class InputHandler
     {
     public:
         InputHandler();
 
-        bool Pressed(std::string input);
-        bool Held(std::string input);
-        bool Released(std::string input);
+        bool Pressed(InputActions input);
+        bool Held(InputActions input);
+        bool Released(InputActions input);
+
+        void Set(InputActions action, std::vector<int> keys);
         
         void HandleInput(GLFWwindow* window, int key, int scancode, int action, int mods);
     private:
-        std::map<std::string, std::vector<int>> keyboardAliases;
-        std::map<std::string, std::vector<int>> gamepadAliases;
-        std::map<std::string, int> keyStates;
+        std::map<InputActions, std::vector<int>> keyboardAliases;
+        std::map<InputActions, std::vector<int>> gamepadAliases;
+        std::map<InputActions, int> keyStates;
     };
 }
 
--- src/Input.cpp
+++ src/Input.cpp
@@ -9,38 +9,67 @@ namespace UT
         // Set up some common aliases
         keyboardAliases =
         {
-            {"confirm", {GLFW_KEY_Z, GLFW_KEY_ENTER}},
-            {"back", {GLFW_KEY_X, GLFW_KEY_LEFT_SHIFT, GLFW_KEY_RIGHT_SHIFT}},
-            {"menu", {GLFW_KEY_C, GLFW_KEY_LEFT_CONTROL, GLFW_KEY_RIGHT_CONTROL}},
-            {"exit", {GLFW_KEY_ESCAPE}}
+            {Confirm, {GLFW_KEY_Z, GLFW_KEY_ENTER}},
+            {Back, {GLFW_KEY_X, GLFW_KEY_LEFT_SHIFT, GLFW_KEY_RIGHT_SHIFT}},
+            {Menu, {GLFW_KEY_C, GLFW_KEY_LEFT_CONTROL, GLFW_KEY_RIGHT_CONTROL}},
+            {Exit, {GLFW_KEY_ESCAPE}}
         };
         gamepadAliases =
         {
-            {"confirm", {GLFW_GAMEPAD_BUTTON_CROSS}},
-            {"back", {GLFW_GAMEPAD_BUTTON_CIRCLE}},
-            {"menu", {GLFW_GAMEPAD_BUTTON_START, GLFW_GAMEPAD_BUTTON_TRIANGLE}},
-            {"exit", {GLFW_GAMEPAD_BUTTON_BACK}}
+            {Confirm, {GLFW_GAMEPAD_BUTTON_CROSS}},
+            {Back, {GLFW_GAMEPAD_BUTTON_CIRCLE}},
+            {Menu, {GLFW_GAMEPAD_BUTTON_START, GLFW_GAMEPAD_BUTTON_TRIANGLE}},
+            {Exit, {GLFW_GAMEPAD_BUTTON_BACK}}
         };
     }
 
-    bool InputHandler::Pressed(std::string input)
+    bool InputHandler::Pressed(InputActions input)
     {
-        if (!(input == "confirm" || input == "back" || input == "menu" || input == "exit")) return false;
-
         return keyStates[input] == GLFW_PRESS;
     }
 
-    bool InputHandler::Held(std::string input)
+    bool InputHandler::Held(InputActions input)
     {
-        if (!(input == "confirm" || input == "back" || input == "menu" || input == "exit")) return false;
-
         return keyStates[input] == GLFW_REPEAT;
     }
 
-    bool InputHandler::Released(std::string input)
+    bool InputHandler::Released(InputActions input)
     {
-        if (!(input == "confirm" || input == "back" || input == "menu" || input == "exit")) return false;
         return keyStates[input] == GLFW_RELEASE;
+    }
+
+    void InputHandler::Set(InputActions action, std::vector<int> keys)
+    {
+        std::vector<int> confirmedKeys = {};
+        std::vector<int> confirmedButtons = {};
+        for (int i = 0; i < keys.size(); i++)
+        {
+            int key = keys[i];
+            if (key >= GLFW_GAMEPAD_BUTTON_A && key <= GLFW_GAMEPAD_BUTTON_DPAD_LEFT)
+            {
+                confirmedButtons.push_back(key);
+                continue;
+            }
+
+            if (key < GLFW_KEY_SPACE) continue;
+            if (key < GLFW_KEY_APOSTROPHE && key > GLFW_KEY_SPACE) continue;
+            if (key < GLFW_KEY_COMMA && key > GLFW_KEY_APOSTROPHE) continue;
+            if (key == 58) continue;
+            if (key == 60) continue;
+            if (key < GLFW_KEY_A && key > GLFW_KEY_EQUAL) continue;
+            if (key < GLFW_KEY_GRAVE_ACCENT && key > GLFW_KEY_RIGHT_BRACKET) continue;
+            if (key < GLFW_KEY_WORLD_1 && key > GLFW_KEY_GRAVE_ACCENT) continue;
+            if (key < GLFW_KEY_ESCAPE && key > GLFW_KEY_WORLD_2) continue;
+            if (key < GLFW_KEY_CAPS_LOCK && key > GLFW_KEY_END) continue;
+            if (key < GLFW_KEY_F1 && key > GLFW_KEY_PAUSE) continue;
+            if (key < GLFW_KEY_KP_0 && key > GLFW_KEY_F25) continue;
+            if (key < GLFW_KEY_LEFT_SHIFT && key > GLFW_KEY_KP_EQUAL) continue;
+            if (key > GLFW_KEY_MENU) continue;
+            confirmedKeys.push_back(key);
+        }
+
+        if (confirmedKeys.size() > 0) keyboardAliases[action] = confirmedKeys;
+        if (confirmedButtons.size() > 0) gamepadAliases[action] = confirmedButtons;
     }
 
     void InputHandler::HandleInput(GLFWwindow* window, int key, int scancode, int action, int mods)
