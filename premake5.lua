2da1632a8a5e5c14161e457eb683074a9c2f69d0
PeriBooty <peribooty@pm.me>
2019-08-01 11:59:08 -0400

    Moved to premake and SFML [CRASHES]
    Signed-off-by: PeriBooty <peribooty@pm.me>

---
+++ premake5.lua
@@ -0,0 +1,80 @@
+include "conanbuildinfo.premake.lua"
+
+workspace "UndertaleEngine"
+    conan_basic_setup()
+    architecture "x64"
+    startproject "example"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+    project "utengine"
+        kind "StaticLib"
+        language "C++"
+        cppdialect "C++17"
+        staticruntime "on"
+
+        targetdir "build/bin/%{prj.name}-%{cfg.buildcfg}"
+        objdir "build/obj/%{prj.name}-%{cfg.buildcfg}"
+        linkoptions { conan_exelinkflags }
+
+        files
+        {
+            "includes/**.h",
+            "src/**.cpp"
+        }
+
+        includedirs
+        {
+            "includes"
+        }
+
+        filter "configurations:Debug"
+            defines "UT_DEBUG"
+            runtime "Debug"
+            symbols "on"
+        
+        filter "configurations:Release"
+            defines "UT_RELEASE"
+            runtime "Release"
+            optimize "on"
+
+    project "Sandbox"
+        location "Sandbox"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++17"
+        staticruntime "on"
+
+        targetdir "build/bin/%{prj.name}-%{cfg.buildcfg}"
+        objdir "build/obj/%{prj.name}-%{cfg.buildcfg}"
+
+        files
+        {
+            "%{prj.name}/includes/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+
+        includedirs
+        {
+            "%{prj.name}/includes",
+            "includes/"
+        }
+
+        links
+        {
+            "utengine"
+        }
+
+        filter "configurations:Debug"
+            defines "UT_DEBUG"
+            runtime "Debug"
+            symbols "on"
+        
+        filter "configurations:Release"
+            defines "UT_RELEASE"
+            runtime "Release"
+            optimize "on"
