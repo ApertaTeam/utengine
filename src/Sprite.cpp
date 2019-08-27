0825fc23ffc4249d0ee692c22b65f78c2f79b611
PeriBooty <peribooty@pm.me>
2019-08-27 17:27:23 -0400

    Made a slightly clearer adjustment to the bug fix for the Sprite class

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -18,8 +18,8 @@ namespace UT
     void Sprite::draw(sf::RenderTarget& target, sf::RenderStates states) const
     {
         states.transform *= getTransform();
-        sf::Vector2f position(states.transform.getMatrix()[12], states.transform.getMatrix()[13]);
-        sf::FloatRect transformRect(position, sf::Vector2f(GetTextureRect().width, GetTextureRect().height));
+        sf::FloatRect transformRect({0, 0}, sf::Vector2f(GetTextureRect().width, GetTextureRect().height));
+        transformRect = states.transform.transformRect(transformRect);
         sf::VertexArray arr(sf::Quads, 4);
         arr[0].position = sf::Vector2f(transformRect.left, transformRect.top);
         arr[1].position = sf::Vector2f(transformRect.left + transformRect.width, transformRect.top);
