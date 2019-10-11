816c3e09f26e1d19f85368909afa9911ce46f937
MadCreativity <ahlgreenmadsen@gmail.com>
2019-10-11 11:19:04 +0200

    Add explanations back in, but keep prior location of syntax explanation.

--- includes/Rectangle9Slice.h
+++ includes/Rectangle9Slice.h
@@ -7,25 +7,44 @@
 
 namespace UT
 {
-    /** Define rectangle points in this order: left, top, right, bottom
+    /** Class used for drawing sections of 9 slices
+    * Middle slices are repeated until the slices fit the size of a rectangle
+    * Define rectangle points in this order: left, top, right, bottom
     * The higher the speed, the faster the transition */
     class Rectangle9Slice : public sf::Drawable
     {
     public:
+        /** Default constructor */
         Rectangle9Slice() : slice(), rect() {}
+
+        /** Constructor that initially sets array of sprite slices */
         Rectangle9Slice(std::array<Sprite, 9> slice) : slice(slice), rect() {};
+
+        /** Constructor that initially sets rectangle points */
         Rectangle9Slice(sf::IntRect rect) : slice(), rect() {};
+
+        /** Constructor that initially sets rectangle points and array of sprite slices */
         Rectangle9Slice(sf::IntRect rect, std::array<Sprite, 9> slice) : slice(slice), rect(rect) {};
 
 
+        /** Transitionally moves rectangle points to new rectangle points */
         void Update();
 
 
+        /** Sets array of sprites used as slices */
         inline void SetSlice(std::array<Sprite, 9> slice) { this->slice = slice; }
+
+        /** Instantly defines rectangle points */
         inline void SetRect(sf::IntRect rect) { this->rect = rect; }
+
+        /** Returns array of sprites used as slices */
         inline const std::array<Sprite, 9> GetSlice() const { return this->slice; }
+
+        /** Returns rectangle points */
         inline const sf::IntRect GetRect() const { return this->rect; }
 
+
+        /** Begins the transition from the current points to the new points */
         void MoveToRect(sf::IntRect rect, int speed = 40);
 
     private:
