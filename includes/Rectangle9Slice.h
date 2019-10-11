89d1e9db3d7c87d9c826e883b4ae6955ce74dae3
MadCreativity <ahlgreenmadsen@gmail.com>
2019-10-11 10:53:11 +0200

    Add documentation for Rectangle9Slice class.

--- includes/Rectangle9Slice.h
+++ includes/Rectangle9Slice.h
@@ -7,21 +7,42 @@
 
 namespace UT
 {
+    /** Class used for drawing sections of 9 slices
+    * Middle slices are repeated until the slices fit the size of a rectangle */
     class Rectangle9Slice : public sf::Drawable
     {
     public:
+        /** Default constructor */
         Rectangle9Slice() : slice(), rect() {}
+
+        /** Constructor that initially sets array of sprite slices */
         Rectangle9Slice(std::array<Sprite, 9> slice) : slice(slice), rect() {};
+
+        /** Constructor that initially sets rectangle points */
         Rectangle9Slice(sf::IntRect rect) : slice(), rect() {};
+
+        /** Constructor that initially sets rectangle points and array of sprite slices */
         Rectangle9Slice(sf::IntRect rect, std::array<Sprite, 9> slice) : slice(slice), rect(rect) {};
 
+
+        /** Transitionally moves rectangle points to new rectangle points*/
         void Update();
 
+
+        /** Sets array of sprites used as slices */
         inline void SetSlice(std::array<Sprite, 9> slice) { this->slice = slice; }
+
+        /** Instantly defines rectangle points */
         inline void SetRect(sf::IntRect rect) { this->rect = rect; }
+
+        /** Returns array of sprites used as slices */
         inline const std::array<Sprite, 9> GetSlice() const { return this->slice; }
+
+        /** Returns rectangle points */
         inline const sf::IntRect GetRect() const { return this->rect; }
 
+
+        /** Begins the transition from the current points to the new points */
         void MoveToRect(sf::IntRect rect, int speed = 2);
 
     private:
@@ -33,6 +54,7 @@ namespace UT
         Vector4f resizeCalculations = {};
         int resizeSpeed = 0;
 
+        /** Renders slices at correct locations relative to defined rectangle points */
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     };
 };
