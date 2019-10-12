291f4d54609a7b567f330abe396bf021ae659458
PeriBooty <peribooty@pm.me>
2019-10-12 14:40:53 -0400

    Added some functionally to the RichText class [UNTESTED]

--- includes/RichText.h
+++ includes/RichText.h
@@ -1,15 +1,33 @@
 #ifndef UT_RICH_TEXT_H
 #define UT_RICH_TEXT_H
 
+#include <string>
+#include "Font.h"
+
 namespace UT
 {
-    class RichText
+    enum TextType
+    {
+        Normal,
+        Wavy,
+        Shakey,
+        WavyShake
+    };
+
+    class RichText : sf::Drawable, sf::Transformable
     {
     public:
         RichText();
 
-    private:
+        const Font const* GetFont() const { return this->font; }
+        void SetFont(Font* font) { this->font = font; }
 
+        std::string rawText;
+        TextType type;
+    private:
+        Font* font;
+
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     };
 }
 
