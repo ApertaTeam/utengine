e07a49599c691476f435ba51b2b7dbad46557c3c
PeriBooty <peribooty@pm.me>
2019-11-19 12:13:16 -0500

    Quick fix for the quick fix

--- src/Door.cpp
+++ src/Door.cpp
@@ -51,6 +51,6 @@ namespace UT
 
     void Door::draw(sf::RenderTarget& target, sf::RenderStates states) const
     {
-        if (!Fader::IsDone()) target.draw(Fader::GetInstance(), states);
+        target.draw(Fader::GetInstance(), states);
     }
 }
