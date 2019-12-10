e161e7ddc541abf65bf6cf135515006ff378fb8f
MadCreativity <ahlgreenmadsen@gmail.com>
2019-12-10 11:57:03 +0100

    Set up SaveHandler structure.

--- includes/SaveHandler.h
+++ includes/SaveHandler.h
@@ -1,1 +1,31 @@
-#pragma once
+#ifndef UT_SAVE_HANDLER_H
+#define UT_SAVE_HANDLER_H
+#include <string>
+#include <map>
+
+namespace UT
+{
+    struct Datatype
+    {
+        union {
+            std::string s;
+            double d;
+            int64_t i_64;
+        };
+    };
+
+    enum class FileEncryption
+    {
+        INI,
+        Binary
+    };
+
+    // Class used for saving and loading data
+    class SaveHandler
+    {
+    public:
+        static void SaveData(std::string filename, std::map<std::string, Datatype> data, FileEncryption encryption);
+        static std::map<std::string, Datatype> LoadData(std::string filename, FileEncryption encryption);
+    };
+}
+#endif
---
+++ src/SaveHandler.cpp
@@ -0,0 +1,14 @@
+#include "SaveHandler.h"
+
+namespace UT
+{
+    void SaveHandler::SaveData(std::string filename, std::map<std::string, Datatype> dataype, FileEncryption encryption)
+    {
+
+    }
+
+    std::map<std::string, Datatype> SaveHandler::LoadData(std::string filename, FileEncryption encryption)
+    {
+
+    }
+}
